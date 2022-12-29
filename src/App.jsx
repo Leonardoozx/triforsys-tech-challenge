9235829fd5b419239b625f19bc1015fec83ee222
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-12-28 22:23:40 -0300

    feat: adds the `Header` component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+
+import './styles/base.css';
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <img id="main-img" src="https://imgur.com/c849jat.png" alt="woman swimming" />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import '../styles/header.css';
+
+export default function Header() {
+  return (
+    <header>
+      {/* 'Unycos' logo */}
+      <img id="logo" src="https://imgur.com/BVHhCTV.png" alt="Unycos logo" />
+      <div>
+        <a href="##">CURSOS</a>
+        <a href="##">REGISTER</a>
+        <a href="##">LOG IN</a>
+      </div>
+    </header>
+  );
+}

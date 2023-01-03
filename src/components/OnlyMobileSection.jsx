840cd3940f93dd25d069d3cde5bb942f8419682d
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-03 20:20:59 -0300

    feat: a section that only appears on mobile devices

---
+++ src/components/OnlyMobileSection.jsx
@@ -0,0 +1,12 @@
+import '../styles/onlyMobileSection.css'
+
+export default function OnlyMobileSection() {
+  return (
+    <section id="mobile-section">
+      <h3>ENTRENAR PARA EL ÉXITO</h3>
+      <p>
+      Fusce sapien lectus, tincidunt scelerisque leo vitae, maximus venenatis felis. Suspendisse potenti. Aenean tincidunt mauris et euismod tincidunt. Aliquam erat volutpat. Cras eu feugiat diam. Suspendisse potenti. Nunc faucibus vulputate neque, sit amet pulvinar mi. Ut lacus neque, imperdiet vitae viverra id, vestibulum.
+      </p>
+    </section>
+  )
+}

92f8cd5c5752a93155d46e4fe5b6a40672bd6262
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-03 19:15:30 -0300

    feat: `Header` for mobile is now working, but not the burguer menu button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import '../styles/header.css';
 
 export default function Header() {
+  const { isUserLoggedIn } = JSON.parse(localStorage.getItem('user')) || {
+    isUserLoggedIn: false,
+  };
   return (
     <header>
       {/* 'Unycos' logo */}
@@ -8,15 +11,19 @@ export default function Header() {
         <img id="logo" src="https://imgur.com/VHl5C3y.png" alt="Unycos logo" />
       </article>
 
-      <div id="menu">
-        <button type="button">
-          <img src="https://imgur.com/Cn6u6tT.png" alt="burguer menu" />
-        </button>
-      </div>
-
-      {/* <article id="menu">
-        <img src="https://imgur.com/mP3jD8i.png" alt="profile" />
-      </article> */}
+      {isUserLoggedIn ? (
+        <div className="menu">
+          <button type="button" onClick={ () => console.log('menu')}>
+            <img src="https://imgur.com/Cn6u6tT.png" alt="burguer menu" />
+          </button>
+        </div>
+      ) : (
+        <div className="menu smaller-profile">
+          <button type="button" onClick={ () => console.log('fazer login')}>
+            <img src="https://imgur.com/mP3jD8i.png" alt="profile" />
+          </button>
+        </div>
+      )}
     </header>
   );
 }

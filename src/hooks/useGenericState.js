5f9a5ac0a1b0aa79951803bf3c8db647581d7052
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-04 01:36:21 -0300

    feat: now the `useGenericState` hook is able to handle with checkbox

--- src/hooks/useGenericState.js
+++ src/hooks/useGenericState.js
@@ -3,7 +3,11 @@ import { useState } from 'react';
 function useGenericState(initial_state) {
   const [genericState, setGenericState] = useState(initial_state);
 
-  const setState = ({ target: { name, value }}) => setGenericState((prevState) => ({ ...prevState, [name]: value }));
+  const setState = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    return setGenericState((prevState) => ({ ...prevState, [name]: value }));
+  };
 
   return [genericState, setState];
 }

192e744d5010a1d0e612daf57b18ddc16bac7d36
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-04 16:28:55 -0300

    .

---
+++ src/api/relatedArticlesData.js
@@ -0,0 +1,23 @@
+// simulating a related articles data fetching
+const relatedArticlesData = () => {
+  const relatedArticles = [
+    {
+      title: 'Consejos para nadadores principiantes',
+      date: '22/06/2019',
+    },
+    {
+      title: 'RECOMENDACIONES PARA LOGRAR QUE UN NIÑO VENZA EL MIEDO AL AGUA',
+      date: '22/06/2019',
+    },
+    {
+      title: '¿Nadar es un buen método para adelgazar?',
+      date: '22/06/2019',
+    },
+    {
+      title: 'Consejos para mejorar la técnica del estilo crol',
+      date: '22/06/2019',
+    },
+  ];
+  return relatedArticles
+};
+export { relatedArticlesData };

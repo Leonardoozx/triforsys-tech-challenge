6f555081eae84f9cf2df14b222991dfb921d8c48
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-04 20:45:41 -0300

    feat: add some data fetching simulations

--- src/api/commentsData.js
+++ src/api/commentsData.js
@@ -3,7 +3,7 @@ const commentsData = () => {
   const comments = [
     {
       content:
-        'Ha sido un curso muy gratificante,en el que he podido aprender muchas cosas gracias a los grandes ponentes que han colaborado y aportado sus conocimientos. Ahora toca ponerse manos a la obra y poner en practica todo lo aprendido. Estoy muy orgulloso.',
+        'Ha sido un curso muy gratificante,en el que he podido aprender muchas cosas gracias a los grandes ponentes que han colaborado y aportado sus conocimientos. Ahora toca ponerse manos a la obra y poner en practica todo lo aprendido. Estoy mas orgulloso.',
       name: 'Samuel F.',
     },
     {
@@ -11,8 +11,18 @@ const commentsData = () => {
         'Seguir creciendo como profesional es necesario pero si lo haces junto a tus ídolos se convierte en un placer. Gracias por esta experiencia y espero poder comentarlo en persona con sus protagonistas muy pronto.',
       name: 'Garcia J.',
     },
+    {
+      content:
+        'Seguir creciendo como profesional es necesario pero si lo haces junto a tus ídolos se convierte en un placer. Gracias por esta experiencia y espero poder comentarlo en persona con sus protagonistas muy pronto.',
+      name: 'Ricardo G.',
+    },
+    {
+      content:
+        'Ha sido un curso muy gratificante,en el que he podido aprender muchas cosas gracias a los grandes ponentes que han colaborado y aportado sus conocimientos. Ahora toca ponerse manos a la obra y poner en practica todo lo aprendido. Estoy mas orgulloso.',
+      name: 'Leonardo G.',
+    },
   ];
-  return comments;
+    return comments;
 };
 
 export { commentsData };
---
+++ src/api/moreCoursesData.js
@@ -0,0 +1,17 @@
+const moreCoursesData = () => {
+  const courses = [
+    {
+      courseThumb: 'https://imgur.com/8SVWsNz.png',
+      teacher: 'Marcus Cooper',
+      whatTeaches: 'Piragüismo',
+    },
+    {
+      courseThumb: 'https://imgur.com/Z5jkk7V.png',
+      teacher: 'Lewis Amarante',
+      whatTeaches: 'Maquillaje',
+    },
+  ];
+  return courses;
+};
+
+export { moreCoursesData };
